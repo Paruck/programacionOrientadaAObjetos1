0e9446dfc4ab58b80cdd78668b99b4b366495f9a
Paruck <emiliomalagon504b@gmail.com>
2017-04-19 16:03:00 -0500

    Add files via upload

---
+++ Tarea1/Tarea1/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() 
+{
+	int suma;
+	int y = 0;
+	for (int x = 0; x < 10; ++x) 
+	{
+		suma = x + y;
+		std::cout << "El valor de la suma es igual a : " << suma << std::endl;
+
+		if (++x) 
+		{
+			std::cout << "El valor de X es igual a : " << x << std::endl;
+		}
+
+		// El incremento del ciclo for se realiza al final.
+	}
+	system("PAUSE");
+
+	return 0;
+}
