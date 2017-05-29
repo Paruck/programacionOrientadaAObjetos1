f2b850b9dcab5ac08bd24537a7cbccc0258d024f
Paruck <emiliomalagon504b@gmail.com>
2017-05-29 08:09:51 -0500

    Update Source.cpp

--- Tarea4/ConsoleApplication5/Source.cpp
+++ Tarea4/ConsoleApplication5/Source.cpp
@@ -8,7 +8,7 @@ int convertidorDeBits(int n, int *arreglo) {
 	for (int i = 0; i <= 32; i++) {
 		if ((n % 2 == 0)) { arreglo[i] = 0; }
 		else { arreglo[i] = 1; }
-		n = n / 2;
+		n = n << 2;
 	}
 	return n;
 }
