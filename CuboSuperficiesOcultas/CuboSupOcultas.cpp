9220e94c04485815e1ab4f033ef7b5831e45fa3a
Paruck <emiliomalagon504b@gmail.com>
2017-09-25 16:01:02 -0500

    Update CuboSupOcultas.cpp

--- CuboSuperficiesOcultas/CuboSupOcultas.cpp
+++ CuboSuperficiesOcultas/CuboSupOcultas.cpp
@@ -106,6 +106,33 @@ Mat3 Application::Translation(const float &x, const float & y)
 	return matrizTras;
 }
 
+Mat4 Application::Traslacion(const float & x, const float & y, const float & z)
+{
+	Mat4 matrizTras;
+	matrizTras.m[0][0] = 1;
+	matrizTras.m[0][1] = 0;
+	matrizTras.m[0][2] = 0;
+	matrizTras.m[0][3] = x;
+
+	matrizTras.m[1][0] = 0;
+	matrizTras.m[1][1] = 1;
+	matrizTras.m[1][2] = 0;
+	matrizTras.m[1][3] = y;
+
+	matrizTras.m[2][0] = 0;
+	matrizTras.m[2][1] = 0;
+	matrizTras.m[2][2] = 1;
+	matrizTras.m[2][3] = z;
+
+	matrizTras.m[3][0] = 0;
+	matrizTras.m[3][1] = 0;
+	matrizTras.m[3][2] = 0;
+	matrizTras.m[3][3] = 1;
+
+
+	return matrizTras;
+}
+
 Mat4 Application::RotationX(const float & angulo)
 {
 	Mat4 matrizAngulo;
@@ -128,7 +155,7 @@ Mat4 Application::RotationX(const float & angulo)
 	matrizAngulo.m[3][1] = 0;
 	matrizAngulo.m[3][2] = 0;
 	matrizAngulo.m[3][3] = 1;
-
+	
 	return matrizAngulo;
 }
 
@@ -149,7 +176,7 @@ Mat4 Application::RotationY(const float & angulo)
 	matrizAngulo.m[2][1] = 0;
 	matrizAngulo.m[2][2] = cos(angulo);
 	matrizAngulo.m[2][3] = 0;
-
+	
 	matrizAngulo.m[3][0] = 0;
 	matrizAngulo.m[3][1] = 0;
 	matrizAngulo.m[3][2] = 0;
@@ -189,13 +216,25 @@ std::vector<vect4> Application::crearCubo()
 	std::vector<vect4> vertices1;
 
 	vect4 a = vect4(cx - 100.0, cy - 100.0, 100.0);
+
 	vect4 b = vect4(cx + 100.0, cy - 100.0, 100.0);
+
 	vect4 c = vect4(cx + 100.0, cy + 100.0, 100.0);
+
 	vect4 d = vect4(cx - 100.0, cy + 100.0, 100.0);
+
 	vect4 e = vect4(cx - 100.0, cy - 100.0, -100.0);
+
 	vect4 f = vect4(cx + 100.0, cy - 100.0, -100.0);
 	vect4 g = vect4(cx + 100.0, cy + 100.0, -100.0);
+
 	vect4 h = vect4(cx - 100.0, cx + 100.0, -100.0);
+	
+
+
+
+
+
 	vertices1.push_back(a);
 	vertices1.push_back(b);
 	vertices1.push_back(c);
@@ -206,6 +245,95 @@ std::vector<vect4> Application::crearCubo()
 	vertices1.push_back(h);
 	return vertices1;
 }
+
+vect4 Application::Normalize(const vect4 & v)
+{
+	float distance = sqrt((v.v[0] * v.v[0]) +
+		(v.v[1] * v.v[1]) +
+		(v.v[2] * v.v[2]));
+	if (distance != 0)
+		return vect4(v.v[0] / distance, v.v[1] / distance, v.v[2] / distance);
+	else
+		return v;
+}
+
+Mat4 Application::LookAt(vect4 eye, vect4 target, vect4 up)
+{
+	vect4 rest = Resta(target, eye);
+	vect4 ejez = Normalize(rest);
+	vect4 ejex = Normalize(Cross(ejez, up));
+	vect4 ejey = Normalize(Cross(ejex, ejez));
+	//Mat4 RotY = RotationY(rad); esto no va
+	Mat4 rot;
+	rot.m[0][0] = ejex.v[0];  rot.m[0][1] = ejex.v[1];   rot.m[0][2] = ejex.v[2];   rot.m[0][3] = 0;  //renglon 1
+	rot.m[1][0] = ejey.v[0];  rot.m[1][1] = ejey.v[1];   rot.m[1][2] = ejey.v[2];   rot.m[1][3] = 0;  //renglon 2
+	rot.m[2][0] = ejez.v[0];  rot.m[2][1] = ejez.v[1];   rot.m[2][2] = ejez.v[2];   rot.m[2][3] = 0;  //renglon 4
+	rot.m[3][0] = 0;		rot.m[3][1] = 0;   rot.m[3][2] = 0;   rot.m[3][3] = 1;  //renglon 4
+
+
+	Mat4 translate = Traslacion(-eye.v[0], -eye.v[1], -eye.v[2]);
+	return translate * rot;
+}
+
+vect4 Application::Resta(const vect4 & v1, const vect4 & v2)
+{
+	vect4 vecRes;
+	vecRes.v[0] = v1.v[0] - (v2.v[0]);
+	vecRes.v[1] = v1.v[1] - (v2.v[1]);
+	vecRes.v[2] = v1.v[2] - (v2.v[2]);
+	return vecRes;
+}
+Mat4 Application::Perspectiva(float fovyInDegrees, float aspectRatio, float znear, float zfar)
+{
+	float ymax, xmax;
+	float temp, temp2, temp3, temp4;
+	ymax = znear * tanf(fovyInDegrees *PI / 360.0);
+	//ymin = -ymax;
+	//xmin = -ymax * aspectRatio;
+	xmax = ymax * aspectRatio;
+	Mat4 m = glhFrustumf2(-xmax, xmax, -ymax, ymax, znear, zfar);
+	return m;
+}
+
+Mat4 Application::glhFrustumf2(float left, float right, float bottom, float top, float znear, float zfar)
+{
+	Mat4 matrix;
+	float temp, temp2, temp3, temp4;
+	temp = 2.0 * znear;
+	temp2 = right - left;
+	temp3 = top - bottom;
+	temp4 = zfar - znear;
+	matrix.m[0][0] = temp / temp2;
+	matrix.m[0][1] = 0.0;
+	matrix.m[0][2] = 0.0;
+	matrix.m[0][3] = 0.0;
+
+
+	matrix.m[1][0] = 0.0;
+	matrix.m[1][1] = temp / temp3;
+	matrix.m[1][2] = 0.0;
+	matrix.m[1][3] = 0.0;
+
+
+	matrix.m[2][0] = (right + left) / temp2;
+	matrix.m[2][1] = (top + bottom) / temp3;
+	matrix.m[2][2] = (-zfar - znear) / temp4;
+	matrix.m[2][3] = -1.0;
+
+	matrix.m[3][0] = 0.0;
+	matrix.m[3][1] = 0.0;
+	matrix.m[3][2] = (-temp * zfar) / temp4;
+	matrix.m[3][3] = 0.0;
+	return matrix;
+}
+
+int Application::Point(vect4 & v1, vect4 & v2)
+{
+	int a;
+	a = v1.v[0] * v2.v[0] + (v1.v[1] * v2.v[1] + (v1.v[2] * v1.v[2]));
+	return a;
+}
+
 
 //Mat4 Application::LookAtRh(vect3 eye, vect3 target, vect3 up)
 //{
@@ -222,6 +350,7 @@ void Application::setUp() {
 
 void Application::update()
 {
+	
 	float contador = 1.0f;
 	x += contador;
 	angulo = 0;
@@ -233,29 +362,46 @@ void Application::update()
 	RotateMatrix = Rotation(rad);
 	TransMatrxi = Translation(y, x);
 	Final = mtrx * trans2 * RotateMatrix * trans1;
-	verticesT.clear();
 	/*for (vect3 &v : vertices) {
-	v1 = mtrx.multi(v, Final);
-	verticesT.push_back(v1);
+		v1 = mtrx.multi(v, Final);
+		verticesT.push_back(v1);
 	}*/
 	Mat4 RotX = RotationX(rad);
 	Mat4 RotY = RotationY(rad);
 	Mat4 RotZ = RotationZ(rad);
-
-	Mat4 Final1 = RotY;
+	//(float fovyInDegrees, float aspectRatio, float znear, float zfar)
+	Mat4 perspectiva = Perspectiva(45, WIDTH/HEIGHT, 1, 100); //esto esta mal
 
 	Mat4 m1;
 	vect4 v2;
+	Mat4 Trans1, Trans2,Trans3;
+	Trans1 = Traslacion(-255, -255, 1);
+	Trans2 = Traslacion(255, 255, 1);
+	Trans3 = Traslacion(170, 170, 1);
 
+	eye = vect4(15.0, 15.0, 15.0);
+	target = vect4(0.0, 0.0, 0.0);
+	up = vect4(0.0, 1.0, 0.0);
+	//Aqui los parametros deben ser Ojo, Target, Up
+	Mat4 Look = LookAt(eye, target, up); //Solo recibe 3 
+	//Mat4 Look = LookAt(vertices1.at(0), vertices1.at(1), vertices1.at(2)); //esto esta mal. 
+	//Mat4 Final1 =Trans2* Trans3* perspectiva *Look*RotY*RotZ*Trans1; //esto tambien esta mal. Tendria que ser algo como :
+	Mat4 Final1 = Look*RotY*RotX * Trans1;
 	verticesF.clear();
 	for (vect4 &v : vertices1) {
 		v2 = m1.multiP(v, Final1);
 		verticesF.push_back(v2);
 	}
+
 }
 
 void Application::draw()
 {
+
+	clearScreen();
+	setColor(0, 255, 132, 0);
+	FUNCIONCHINGONA();
+
 	//int x0 = 255,
 	//	y0 = 255,
 	//	x1 = 200,
@@ -272,9 +418,7 @@ void Application::draw()
 
 	//setColor(0, 255, 255, 0);
 	//circle( 100);
-	clearScreen();
-	setColor(0, 255, 132, 0);
-	FUNCIONCHINGONA();
+	//FUNCIONCHINGONA();
 
 	//for (int i = vertices1.size(); i--;)
 	//{
